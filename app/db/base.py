62935a617c8a9db28222a0d75fd20e91af479dcd
faidhfeisal <133899965+faidhfeisal@users.noreply.github.com>
2025-04-20 20:49:57 +0400

    Update base.py to include all models

--- app/db/base.py
+++ app/db/base.py
@@ -2,7 +2,7 @@
 from app.db.session import Base
 
 # Import models
-# from app.models.user_session import UserSession
-# from app.models.user_profile import UserProfile
-# from app.models.user_statistics import UserStatistics
-# from app.models.expertise_area import ExpertiseArea
+from app.models.user_session import UserSession
+from app.models.user_profile import UserProfile, profile_expertise_table
+from app.models.user_statistics import UserStatistics
+from app.models.expertise_area import ExpertiseArea
